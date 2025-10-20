256f7a239a30e201121ff050cbe4dfa794c80bca
FarhadAhmadi <farhadahmadi1413@gmail.com>
2025-10-20 21:33:25 +0330

    update

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus: string = 'online';
+export class ServerStatusComponent implements OnInit , OnChanges , AfterViewInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+
+  constructor() {}
+  ngAfterViewInit(): void {
+    console.log('ngOnChangesngAfterViewInit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
